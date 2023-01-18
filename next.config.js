ea2c24524704dcd09ba112760b0da0d629af26b0
Dalila <avdukicdalila@gmail.com>
2023-01-18 14:18:21 +0100

    add base path

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  basePath: "/mui-and-next-exercise",
+  assetPrefix: "/mui-and-next-exercise",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
