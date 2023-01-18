3e917c47e0835f5ecddb3f58d4ee39ae8fb99cfd
Dalila <avdukicdalila@gmail.com>
2023-01-18 15:24:07 +0100

    add redirect to basePath

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   swcMinify: true,
   basePath: "/mui-and-next-exercise",
   assetPrefix: "/mui-and-next-exercise",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/mui-and-next-exercise",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
